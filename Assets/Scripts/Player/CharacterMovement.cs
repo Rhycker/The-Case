fc9223203d19c0076ec4a9b33b0af4bc14b45911
Nathanovich <n.flier@live.nl>
2019-09-19 16:22:53 +0200

    Made it so the visuals flip if the character moves in a different direction

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -3,9 +3,13 @@
 public class CharacterMovement : MonoBehaviour {
 
 	[SerializeField] private float movementSpeed;
-	
+	[SerializeField] private SpriteRenderer visuals;
+
 	private void Update() {
 		float movementInput = GameInput.Instance.InputService.Horizontal();
+		if(movementInput == 0) { return; }
+
+		visuals.flipX = movementInput < 0f;
 		Vector2 movement = new Vector2();
 		movement.x = movementInput * movementSpeed * Time.deltaTime;
 		transform.Translate(movement);
