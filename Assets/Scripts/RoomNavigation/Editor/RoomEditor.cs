1e7043e25023e258f0461ed8ad5643b08200a7c0
Nathanovich <n.flier@live.nl>
2019-09-19 15:15:30 +0200

    Created a system for visualizing and positioning the camera to the room that the player enters

---
+++ Assets/Scripts/RoomNavigation/Editor/RoomEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Room))]
+public class RoomEditor : Editor {
+
+	private void OnSceneGUI() {
+		Camera mainCamera = Camera.main;
+		float height = Camera.main.orthographicSize * 2;
+		float width = Camera.main.aspect * height;
+		Vector3 cameraViewSize = new Vector3(width, height, 0f);
+
+		SerializedProperty positionProperty = serializedObject.FindProperty("cameraStartPosition");
+		float cameraXOffset = positionProperty.vector2Value.x + width * 0.5f;
+		float cameraYOffset = positionProperty.vector2Value.y + height * 0.5f;
+		Vector3 localPosition = ((Room)target).transform.position;
+		Vector3 cameraViewStart = new Vector3(localPosition.x + cameraXOffset, localPosition.y + cameraYOffset);
+
+		Handles.color = Color.blue;
+		Handles.DrawWireCube(cameraViewStart, cameraViewSize);
+	}
+
+}
---
+++ Assets/Scripts/RoomNavigation/Room.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	[SerializeField] private Vector2 cameraStartPosition;
+
+	public void Enter() {
+		Vector3 cameraPosition = cameraStartPosition;
+		cameraPosition.z = Camera.main.transform.position.z;
+		Camera.main.transform.position = cameraPosition;
+	}
+	
+}
