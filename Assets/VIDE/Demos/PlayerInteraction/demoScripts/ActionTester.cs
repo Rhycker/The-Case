caab98fb99496a66e46245c4da5268e641a188e8
Nathanovich <n.flier@live.nl>
2019-09-27 10:34:58 +0200

    Added VIDE plugin for easily creating narrative

---
+++ Assets/VIDE/Demos/PlayerInteraction/demoScripts/ActionTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionTester : MonoBehaviour {
+
+	private VIDE_Assign vide;
+
+	private void Awake() {
+		vide = GetComponent<VIDE_Assign>();
+	}
+
+	public void SayHello() {
+		Debug.Log("Hello");
+	}
+	
+}
