b65561e96762ad20a8ac489bd7de61c97e8cf6f4
Nathanovich <n.flier@live.nl>
2019-09-27 10:35:38 +0200

    Beginning of VIDE conversation test setup for The Case

---
+++ Assets/Scripts/NPC/NPC_Tom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Get node variables: CyclinderGuy.cs
+// Call methods from script: witchExample.cs
+public class NPC_Tom : MonoBehaviour {
+
+	private void Awake() {
+        
+    }
+	
+}
---
+++ Assets/Scripts/Narrative/Conversation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VIDE_Data;
+
+// Example: VIDEUIManager1.cs
+public class Conversation : MonoBehaviour {
+
+	[SerializeField] private string saveGameName = "Game";
+
+	private void Awake() {
+		// VD.LoadDialogues(); //Load all dialogues to memory so that we dont spend time doing so later
+		VD.LoadDialogues();
+
+		//Loads the saved state of VIDE_Assigns and dialogues.
+		VD.LoadState(saveGameName, true);
+	}
+
+	public void EndDialogue(VD.NodeData data) {
+		//Saves VIDE stuff related to EVs and override start nodes
+		VD.SaveState("VIDEDEMOScene1", true);
+	}
+
+	private void UpdateUI(VD.NodeData data) {
+
+	}
+
+	// NOT READY IMPLEMENTING
+	private void OnDisable() {
+		//If the script gets destroyed, let's make sure we force-end the dialogue to prevent errors
+		//We do not save changes
+		//VD.OnActionNode -= ActionHandler;
+		VD.OnNodeChange -= UpdateUI;
+		VD.OnEnd -= EndDialogue;
+		//if (dialogueContainer != null)
+			//dialogueContainer.SetActive(false);
+		VD.EndDialogue();
+	}
+
+}
