c2bc38300958ee87db32154cd7090e8438bd9d46
Nathanovich <n.flier@live.nl>
2019-10-10 14:28:26 +0200

    Made it so a door editorScript automatically updates the door name to tell the dev where the door goes to and whether it is open or not

---
+++ Assets/Scripts/RoomNavigation/Editor/DoorEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(Door))]
+public class DoorEditor : Editor {
+
+	private Door targetScript;
+	private Room selectedRoom;
+	private bool doorIsOpen = true;
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+
+		targetScript = (Door)target;
+		bool newDoorIsOpen = serializedObject.FindProperty("isOpen").boolValue;
+
+		if (targetScript.TargetRoom != selectedRoom || newDoorIsOpen != doorIsOpen) {
+			selectedRoom = targetScript.TargetRoom;
+			doorIsOpen = newDoorIsOpen;
+
+			string doorName = "DoorTo:" + targetScript.TargetRoom.name;
+			if (!newDoorIsOpen) {
+				doorName = doorName + " (closed)";
+			}
+			targetScript.transform.name = doorName;
+		}
+	}
+
+}
