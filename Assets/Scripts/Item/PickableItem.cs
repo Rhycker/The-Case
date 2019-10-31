f34a898f2991f0d0e03db35ef8bc002b3fd39a5a
Nathanovich <n.flier@live.nl>
2019-10-31 16:53:38 +0100

    Added pickable item logic, created three pickable items

---
+++ Assets/Scripts/Item/PickableItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PickableItem : InteractableObject {
+
+	[AssetDropdown("Items", typeof(Item))][SerializeField] private Item item;
+
+	public override void Interact() {
+		InventoryPanel.Instance.AddItemWidget(item);
+		Destroy(gameObject);
+	}
+	
+}
