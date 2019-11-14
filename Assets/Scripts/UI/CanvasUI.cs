20587d2a9dc1f493f80f14f1bea2cb29baba02de
Nathanovich <n.flier@live.nl>
2019-11-14 12:59:02 +0100

    Missing stages for the previous commit

---
+++ Assets/Scripts/UI/CanvasUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class CanvasUI : MonoBehaviour {
+
+	private Canvas canvas;
+	
+	public void ShowCanvas(bool show) {
+		if (canvas == null) {
+			canvas = GetComponent<Canvas>();
+		}
+		canvas.enabled = show;
+	}
+
+	protected virtual void Awake() {
+		ShowCanvas(true);
+    }
+	
+}
---
+++ Assets/Scripts/UI/Editor/CanvasUIEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(CanvasUI), true)]
+public class CanvasUIEditor : Editor {
+
+	private void OnEnable() {
+		if (EditorApplication.isPlaying) { return; }
+		CanvasUI targetScript = (CanvasUI)target;
+		targetScript.ShowCanvas(true);
+	}
+
+	private void OnDisable() {
+		CanvasUI targetScript = (CanvasUI)target;
+		if(targetScript == null) { return; }
+		if(Selection.gameObjects.Length == 0) {
+			targetScript.ShowCanvas(false);
+			return;
+		}
+
+		if(Selection.activeTransform.root != targetScript.transform) {
+			targetScript.ShowCanvas(false);
+		}
+	}
+
+}
