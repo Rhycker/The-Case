0a3ac44df870a9432648ae9d7560dcc3777df0dd
Rhycker <50843484+Rhycker@users.noreply.github.com>
2019-12-11 22:20:35 +0100

    Fire sound added, Houthakker fixed spritesheet, audio fire has 3D effect

--- Assets/Scripts/Interactable/Fire.cs
+++ Assets/Scripts/Interactable/Fire.cs
@@ -3,15 +3,18 @@
 [RequireComponent(typeof(Animator))]
 public class Fire : InteractableObject {
 
-	public bool IsLit { get; private set; }
+    [SerializeField] private AudioClip interactionSound;
+    public bool IsLit { get; private set; }
 
 	[AssetDropdown("Items", typeof(Item))] [SerializeField] private Item requiredItem;
 	private Animator animator;
 
 	public override void UseItem(Item item) {
 		if(item == requiredItem) {
-			// Ignite fire
-			Inventory.Instance.RemoveItem(item);
+            // Ignite fire sound
+            SoundManager.Instance.PlaySound(interactionSound);
+            // Ignite fire
+            Inventory.Instance.RemoveItem(item);
 			IsLit = true;
 			animator.enabled = true;
 			CanInteract = false;
